b3d7d18b5388df1909b5944ecd9ed9881073757c
Pangeran Alvins <48967817+PangeranAlvins@users.noreply.github.com>
2019-08-07 11:30:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
 pkg install python3;pkg install php;pip install bs4;pip install requests;pip install mechanize;pkg install openssh;pkg install php;pip2 install -r requirements.txt
+pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;git clone https://github.com/PangeranAlvins/Alvins-Tools;cd Alvins-Tools;python3 Alvins-Tools.py
